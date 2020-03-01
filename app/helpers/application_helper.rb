8465c77546fa8e857bc464020ccca1f282161d66
taimushimizu <213.f.r48132@gmail.com>
2020-03-01 11:33:12 +0900

    ページ毎にタイトルが変わるよう実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "11ちゃんねる"
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
